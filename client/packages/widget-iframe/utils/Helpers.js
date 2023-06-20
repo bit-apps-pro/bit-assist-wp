a6a36523cd9cb6924764e517c6619a0bb1f8316a
Shimul Chakraborty <shimul.ckbt@gmail.com>
2023-06-20 19:32:42 +0600

    refactor: helper functions

--- client/packages/widget-iframe/utils/Helpers.js
+++ client/packages/widget-iframe/utils/Helpers.js
@@ -11,41 +11,23 @@ export const createElm = (elm, attributes) => {
 	return domElm
 }
 
-export const globalAppend = (elm, child) => {
-	Array.isArray(child) ? elm.append(...child) : elm.append(child)
-}
+export const globalAppend = (elm, child) => (Array.isArray(child) ? elm.append(...child) : elm.append(child))
 
-export const globalEventListener = (selector, type, callback) => {
-	selector.addEventListener(type, callback)
-}
+export const globalEventListener = (selector, type, callback) => selector.addEventListener(type, callback)
 
-export const globalSetProperty = (selector, propName, value) => {
-	selector.setProperty(propName, value)
-}
+export const globalSetProperty = (selector, propName, value) => selector.setProperty(propName, value)
 
-export const globalPostMessage = (parent, message, clientDomain) => {
-	parent.postMessage(message, clientDomain)
-}
+export const globalPostMessage = (parent, message, clientDomain) => parent.postMessage(message, clientDomain)
 
-export const globalQuerySelectorAll = (documentTarget, selectedClass) => {
-	return documentTarget.querySelectorAll(selectedClass)
-}
+export const globalQuerySelectorAll = (documentTarget, selectedClass) => documentTarget.querySelectorAll(selectedClass)
 
-export const globalClassListRemove = (selector, action) => {
-	return selector?.classList.remove(action)
-}
+export const globalClassListRemove = (selector, action) => selector?.classList.remove(action)
 
-export const globalClassListAdd = (selector, action) => {
-	return selector?.classList.add(action)
-}
+export const globalClassListAdd = (selector, action) => selector?.classList.add(action)
 
-export const globalClassListContains = (selector, action) => {
-	return selector?.classList.contains(action)
-}
+export const globalClassListContains = (selector, action) => selector?.classList.contains(action)
 
-export const globalClassListToggle = (selector, action) => {
-	return selector?.classList.toggle(action)
-}
+export const globalClassListToggle = (selector, action) => selector?.classList.toggle(action)
 
 export const globalSetAttribute = (domElm, attribute, value) => {
 	domElm.setAttribute(attribute, value)
