858bab38fbdae798f0ad15b21f6499891bebe3a5
Shimul Chakraborty <shimul.ckbt@gmail.com>
2023-07-20 15:32:08 +0600

    fix: sanitization complete

--- backend/app/HTTP/Controllers/WidgetChannelController.php
+++ backend/app/HTTP/Controllers/WidgetChannelController.php
@@ -2,12 +2,12 @@
 
 namespace BitApps\Assist\HTTP\Controllers;
 
-use BitApps\AssistPro\Config as ProConfig;
-use BitApps\Assist\Core\Http\Response;
 use BitApps\Assist\Core\Http\Request\Request;
+use BitApps\Assist\Core\Http\Response;
 use BitApps\Assist\HTTP\Requests\WidgetChannelStoreRequest;
 use BitApps\Assist\HTTP\Requests\WidgetChannelUpdateRequest;
 use BitApps\Assist\Model\WidgetChannel;
+use BitApps\AssistPro\Config as ProConfig;
 
 final class WidgetChannelController
 {
@@ -21,12 +21,14 @@ final class WidgetChannelController
         if ($widgetChannel->exists()) {
             return $widgetChannel;
         }
+
         return Response::error($widgetChannel);
     }
 
     public function store(WidgetChannelStoreRequest $request)
     {
-        $validated = $request->all();
+        $validated = $this->sanitizeRequest($request->all());
+
         $isPro = class_exists(ProConfig::class) && ProConfig::isPro();
 
         if (!$isPro && WidgetChannel::where('widget_id', $validated['widget_id'])->count() >= 2) {
@@ -44,13 +46,15 @@ final class WidgetChannelController
         if ($result) {
             return Response::success('Channel created successfully');
         }
+
         return Response::error('Something went wrong');
     }
 
     public function update(WidgetChannelUpdateRequest $request, WidgetChannel $widgetChannel)
     {
-        $validated = $request->all();
-        $isPro = class_exists(ProConfig::class) && ProConfig::isPro();
+        $validated = $this->sanitizeRequest($request->all());
+
+        $isPro     = class_exists(ProConfig::class) && ProConfig::isPro();
 
         if (!$isPro && !empty($validated['config']['hide_after_office_hours'])) {
             unset($validated['config']['hide_after_office_hours']);
@@ -64,6 +68,7 @@ final class WidgetChannelController
         if ($widgetChannel->save()) {
             return Response::success('Channel updated successfully');
         }
+
         return Response::error('Something went wrong');
     }
 
@@ -78,8 +83,8 @@ final class WidgetChannelController
     {
         foreach ($request->widgetChannels as $widgetChannel) {
             WidgetChannel::take(1)->find($widgetChannel['id'])
-            ->update(['sequence' => $widgetChannel['sequence']])
-            ->save();
+                ->update(['sequence' => $widgetChannel['sequence']])
+                ->save();
         }
 
         return Response::success('Sequence ordered');
@@ -94,23 +99,95 @@ final class WidgetChannelController
 
         if ($widgetChannel->exists()) {
             $newWidgetChannel = $this->replicate($widgetChannel);
-            $result = WidgetChannel::insert((array)$newWidgetChannel);
+            $result           = WidgetChannel::insert((array) $newWidgetChannel);
             if ($result) {
                 return Response::success('Channel copied successfully');
             }
         }
+
         return Response::error('Something went wrong');
     }
 
     private function replicate($widgetChannel)
     {
-        $newWidgetChannel = (object)[];
-        $newWidgetChannel->widget_id = $widgetChannel->widget_id;
-        $newWidgetChannel->channel_name = $widgetChannel->channel_name;
-        $newWidgetChannel->config = $widgetChannel->config;
+        $newWidgetChannel                = (object) [];
+        $newWidgetChannel->widget_id     = $widgetChannel->widget_id;
+        $newWidgetChannel->channel_name  = $widgetChannel->channel_name;
+        $newWidgetChannel->config        = $widgetChannel->config;
         $newWidgetChannel->config->title = $widgetChannel->config->title . ' (Copy)';
-        $newWidgetChannel->sequence = WidgetChannel::where('widget_id', $widgetChannel->widget_id)->max('sequence') + 1;
-        $newWidgetChannel->status = $widgetChannel->status;
+        $newWidgetChannel->sequence      = WidgetChannel::where('widget_id', $widgetChannel->widget_id)->max('sequence') + 1;
+        $newWidgetChannel->status        = $widgetChannel->status;
+
         return $newWidgetChannel;
+    }
+
+    private function sanitizeRequest($validated)
+    {
+        if ($validated['channel_name'] === 'Google-Map') {
+            return $this->sanitizeIframe($validated);
+        } elseif ($validated['channel_name'] === 'Custom-Channel') {
+            return $this->sanitizeUrl($validated);
+        } elseif ($validated['channel_name'] === 'Custom-Iframe') {
+            return $this->sanitizeUrl($validated);
+        } elseif ($validated['channel_name'] === 'FAQ' || $validated['channel_name'] === 'Knowledge-Base') {
+            return $this->sanitizeTitle($validated, $validated['channel_name']);
+        }
+
+        return $validated;
+    }
+
+    private function sanitizeIframe($validated)
+    {
+        $allowedAttributes = [
+            'iframe' => [
+                'src'             => [],
+                'width'           => [],
+                'height'          => [],
+                'style'           => [],
+                'allowfullscreen' => [],
+                'loading'         => [],
+                'referrerpolicy'  => [],
+            ],
+        ];
+
+        $validated['config']['unique_id'] = wp_kses($validated['config']['unique_id'], $allowedAttributes);
+
+        return $validated;
+    }
+
+    private function sanitizeUrl($validated)
+    {
+        $validated['config']['unique_id'] = sanitize_url($validated['config']['unique_id']);
+        $validated['config']['url']       = sanitize_url($validated['config']['url']);
+
+        return $validated;
+    }
+
+    private function sanitizeTitle($validated, $channelName)
+    {
+        $validated['config']['title'] = sanitize_text_field($validated['config']['title']);
+
+        $faqs  = [];
+        $kbs   = [];
+
+        if ($channelName === 'FAQ') {
+            $faqs = &$validated['config']['card_config']['faqs'];
+
+            foreach ($faqs as &$faq) {
+                if (isset($faq['title'])) {
+                    $faq['title'] = sanitize_text_field($faq['title']);
+                }
+            }
+        } else {
+            $kbs = &$validated['config']['card_config']['knowledge_bases'];
+
+            foreach ($kbs as &$kb) {
+                if (isset($kb['title'])) {
+                    $kb['title'] = sanitize_text_field($kb['title']);
+                }
+            }
+        }
+
+        return $validated;
     }
 }
