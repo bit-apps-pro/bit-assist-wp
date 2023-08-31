66be96a10ce24376d7857046a664f9f6da2a35bc
Shimul Chakraborty <shimul.ckbt@gmail.com>
2023-08-31 17:22:51 +0600

    chore: hide credit by default

--- backend/app/HTTP/Controllers/WidgetController.php
+++ backend/app/HTTP/Controllers/WidgetController.php
@@ -2,14 +2,14 @@
 
 namespace BitApps\Assist\HTTP\Controllers;
 
-use BitApps\AssistPro\Config as ProConfig;
 use BitApps\Assist\Config;
-use BitApps\Assist\Core\Http\Response;
 use BitApps\Assist\Core\Http\Request\Request;
+use BitApps\Assist\Core\Http\Response;
 use BitApps\Assist\HTTP\Requests\WidgetStoreRequest;
 use BitApps\Assist\HTTP\Requests\WidgetUpdateRequest;
 use BitApps\Assist\Model\Widget;
 use BitApps\Assist\Model\WidgetChannel;
+use BitApps\AssistPro\Config as ProConfig;
 
 final class WidgetController
 {
@@ -23,6 +23,7 @@ final class WidgetController
         if ($widget->exists()) {
             return $widget;
         }
+
         return Response::error($widget);
     }
 
@@ -47,12 +48,13 @@ final class WidgetController
                 'let'              => 10,
                 'right'            => 10,
                 'badge_active'     => 0,
-                'badge_color'      => ['b'=> 0, 'g'=> 220, 'h'=> 120, 'hex'=> '00dc00', 'r'=> 0, 's'=> 100, 'str'=> '#00dc00', 'v'=> 86],
+                'badge_color'      => ['b' => 0, 'g' => 220, 'h' => 120, 'hex' => '00dc00', 'r' => 0, 's' => 100, 'str' => '#00dc00', 'v' => 86],
                 'animation_active' => 0,
                 'animation_type'   => 1,
                 'widget_show_on'   => ['desktop', 'mobile'],
                 'widget_style'     => 'widget_transparent'
             ],
+            'hide_credit' => 1
         ];
 
         $activeWidget = Config::getOption('widget_active');
@@ -70,12 +72,14 @@ final class WidgetController
 
     public function update(WidgetUpdateRequest $request, Widget $widget)
     {
+        error_log(json_encode($request->all(), JSON_PRETTY_PRINT));
         $widget->update($request->all());
 
         if ($widget->save()) {
             return Response::success('Widget updated');
         }
-        return  Response::error('Widget update failed');
+
+        return Response::error('Widget update failed');
     }
 
     public function destroy(Widget $widget)
@@ -100,7 +104,8 @@ final class WidgetController
 
             return Response::success('Widget status changed');
         }
-        return  Response::error('Widget status not changed');
+
+        return Response::error('Widget status not changed');
     }
 
     public function copy(Widget $widget)
@@ -112,14 +117,16 @@ final class WidgetController
 
         if ($widget->exists()) {
             $newWidget = $this->replicateWidget($widget);
-            $result = Widget::insert((array)$newWidget);
+            $result    = Widget::insert((array) $newWidget);
 
             if ($result) {
                 $widget->with('widgetChannels');
                 $this->copyAllChannels($widget->widgetChannels, $result->id);
+
                 return Response::success('Widget copied successfully');
             }
         }
+
         return Response::error('Something went wrong');
     }
 
@@ -128,38 +135,40 @@ final class WidgetController
         $maxSequence = WidgetChannel::where('widget_id', $widgetId)->max('sequence');
         foreach ($widgetChannels as $widgetChannel) {
             $newWidgetChannel = $this->replicateWidgetChannel($widgetChannel, $widgetId, $maxSequence);
-            WidgetChannel::insert((array)$newWidgetChannel);
-            $maxSequence += 1;
+            WidgetChannel::insert((array) $newWidgetChannel);
+            ++$maxSequence;
         }
     }
 
     private function replicateWidget($widget)
     {
-        $newWidget = (object)[];
-        $newWidget->name = $widget->name . ' (copy)';
-        $newWidget->styles = $widget->styles;
-        $newWidget->business_hours = $widget->business_hours;
-        $newWidget->timezone = $widget->timezone;
-        $newWidget->exclude_pages = $widget->exclude_pages;
-        $newWidget->initial_delay = $widget->initial_delay;
-        $newWidget->page_scroll = $widget->page_scroll;
-        $newWidget->widget_behavior = $widget->widget_behavior;
-        $newWidget->custom_css = $widget->custom_css;
-        $newWidget->call_to_action = $widget->call_to_action;
-        $newWidget->store_responses = $widget->store_responses;
+        $newWidget                   = (object) [];
+        $newWidget->name             = $widget->name . ' (copy)';
+        $newWidget->styles           = $widget->styles;
+        $newWidget->business_hours   = $widget->business_hours;
+        $newWidget->timezone         = $widget->timezone;
+        $newWidget->exclude_pages    = $widget->exclude_pages;
+        $newWidget->initial_delay    = $widget->initial_delay;
+        $newWidget->page_scroll      = $widget->page_scroll;
+        $newWidget->widget_behavior  = $widget->widget_behavior;
+        $newWidget->custom_css       = $widget->custom_css;
+        $newWidget->call_to_action   = $widget->call_to_action;
+        $newWidget->store_responses  = $widget->store_responses;
         $newWidget->delete_responses = $widget->delete_responses;
-        $newWidget->status = $widget->status;
+        $newWidget->status           = $widget->status;
+
         return $newWidget;
     }
 
     private function replicateWidgetChannel($widgetChannel, $widgetId, $maxSequence)
     {
-        $newWidgetChannel = (object)[];
-        $newWidgetChannel->widget_id = $widgetId;
+        $newWidgetChannel               = (object) [];
+        $newWidgetChannel->widget_id    = $widgetId;
         $newWidgetChannel->channel_name = $widgetChannel->channel_name;
-        $newWidgetChannel->config = $widgetChannel->config;
-        $newWidgetChannel->sequence = $maxSequence + 1;
-        $newWidgetChannel->status = $widgetChannel->status;
+        $newWidgetChannel->config       = $widgetChannel->config;
+        $newWidgetChannel->sequence     = $maxSequence + 1;
+        $newWidgetChannel->status       = $widgetChannel->status;
+
         return $newWidgetChannel;
     }
 }
