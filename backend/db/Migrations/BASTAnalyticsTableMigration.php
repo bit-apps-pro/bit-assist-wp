8e385c50184b96ca0740965a092004256795d129
Shimul Chakraborty <shimul.ckbt@gmail.com>
2023-10-11 11:27:47 +0600

    refactor: database column reduced

--- backend/db/Migrations/BASTAnalyticsTableMigration.php
+++ backend/db/Migrations/BASTAnalyticsTableMigration.php
@@ -13,7 +13,6 @@ final class BASTAnalyticsTableMigration extends Migration
     public function up()
     {
         Schema::create('analytics', function (Blueprint $table) {
-            $table->id();
             $table->bigint('widget_id', 20)->unsigned()->foreign('widgets', 'id')->onDelete()->cascade();
             $table->bigint('channel_id', 20)->nullable()->unsigned()->foreign('widget_channels', 'id')->onDelete()->cascade();
             $table->tinyint('is_clicked')->defaultValue(0);
