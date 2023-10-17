d2186488364da0cc92c5bcf3c339635c0c9dbca1
Shimul Chakraborty <shimul.ckbt@gmail.com>
2023-10-17 18:03:28 +0600

    fix: space not working on search channel

--- frontend/src/components/widgetChannels/ChannelSelect.tsx
+++ frontend/src/components/widgetChannels/ChannelSelect.tsx
@@ -7,17 +7,28 @@ import channelList from './ChannelList'
 function ChannelSelect() {
   const [filter, setFilter] = useState<string>('')
 
-  const filteredChannelList = channelList?.filter((channel: Channel) => channel.name.toLowerCase().includes(filter.toLowerCase()))
+  const filterChannels = (channel: Channel, filter: string): boolean => {
+    const words = filter.toLowerCase().split(' ')
+    return words.every((word) => channel.name.toLowerCase().includes(word))
+  }
+
+  const filteredChannelList = channelList?.filter((channel: Channel) => filterChannels(channel, filter))
 
   return (
     <VStack spacing="4">
       <Input value={filter} placeholder="Search" onChange={(e) => setFilter(e.target.value)} />
 
       <Grid justifyContent="center" gap={[2, 3]} w="full" gridTemplateColumns="repeat(auto-fill, minmax(120px, 1fr))">
-        {filteredChannelList?.map((channel: Channel) => <SingleChannel key={channel.name} channel={channel} />)}
+        {filteredChannelList?.map((channel: Channel) => (
+          <SingleChannel key={channel.name} channel={channel} />
+        ))}
       </Grid>
 
-      {filteredChannelList?.length === 0 && <Text color="gray.500" fontSize="md">No item found.</Text>}
+      {filteredChannelList?.length === 0 && (
+        <Text color="gray.500" fontSize="md">
+          No item found.
+        </Text>
+      )}
     </VStack>
   )
 }
