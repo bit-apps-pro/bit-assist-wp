44c85024f934f417196524c21df88d59154a86d9
Shimul Chakraborty <shimul.ckbt@gmail.com>
2023-10-26 18:06:24 +0600

    chore: doc button in external publish section

--- frontend/src/components/publish/Publish.tsx
+++ frontend/src/components/publish/Publish.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Heading, Stack, Text, Tooltip } from '@chakra-ui/react'
+import { Box, Flex, HStack, Heading, Stack, Text, Tooltip } from '@chakra-ui/react'
 import ProWrapper from '@components/global/ProWrapper'
 import Drupal from '@icons/Drupal'
 import Html from '@icons/Html'
@@ -12,13 +12,17 @@ import Weebly from '@icons/Weebly'
 import Wix from '@icons/Wix'
 import CDN from './CDN'
 import Domains from './Domains'
+import DocTutorials from '@components/global/DocTutorials'
 
-interface PlatformProps { children: React.ReactNode, label: string }
+interface PlatformProps {
+  children: React.ReactNode
+  label: string
+}
 
 function Platform({ children, label }: PlatformProps) {
   return (
-    <Tooltip placement='top' hasArrow label={label}>
-      <Flex justifyContent="center" alignItems="center" rounded='lg' h="16" w="16" borderWidth={1} p='4'>
+    <Tooltip placement="top" hasArrow label={label}>
+      <Flex justifyContent="center" alignItems="center" rounded="lg" h="16" w="16" borderWidth={1} p="4">
         {children}
       </Flex>
     </Tooltip>
@@ -28,27 +32,50 @@ function Platform({ children, label }: PlatformProps) {
 function Publish() {
   return (
     <Stack gap={[5, 6]}>
-      <Box textAlign='center'>
-        <Heading as='h4' size="md" mb='4'>
+      <Box textAlign="center">
+        <Heading as="h4" size="md" mb="4">
           Publish Widget on Any Platform
         </Heading>
-        <Flex justifyContent='center' flexWrap='wrap' gap='2'>
-          <Platform label='Shopify'><Shopify /></Platform>
-          <Platform label='Squarespace'><Squarespace /></Platform>
-          <Platform label='Webflow'><Webflow /></Platform>
-          <Platform label='Weebly'><Weebly /></Platform>
-          <Platform label='Wix'><Wix /></Platform>
-          <Platform label='Magento'><Magento /></Platform>
-          <Platform label='Drupal'><Drupal /></Platform>
-          <Platform label='Opencart'><Opencart /></Platform>
-          <Platform label='HTML'><Html /></Platform>
-          <Platform label='More'><More /></Platform>
+        <Flex justifyContent="center" flexWrap="wrap" gap="2">
+          <Platform label="Shopify">
+            <Shopify />
+          </Platform>
+          <Platform label="Squarespace">
+            <Squarespace />
+          </Platform>
+          <Platform label="Webflow">
+            <Webflow />
+          </Platform>
+          <Platform label="Weebly">
+            <Weebly />
+          </Platform>
+          <Platform label="Wix">
+            <Wix />
+          </Platform>
+          <Platform label="Magento">
+            <Magento />
+          </Platform>
+          <Platform label="Drupal">
+            <Drupal />
+          </Platform>
+          <Platform label="Opencart">
+            <Opencart />
+          </Platform>
+          <Platform label="HTML">
+            <Html />
+          </Platform>
+          <Platform label="More">
+            <More />
+          </Platform>
         </Flex>
       </Box>
       <Box>
-        <Text mb={4} py="1" px="3" borderLeft="4px" borderColor="purple.500" fontWeight="medium" fontSize="md">
-          Want to use this widget in other domain? <br /> Follow these steps.
-        </Text>
+        <DocTutorials docLink="https://bitapps.pro/docs/bit-assist/external-publish/" />
+        <HStack justifyContent="space-between">
+          <Text mb={4} py="1" px="3" borderLeft="4px" borderColor="purple.500" fontWeight="medium" fontSize="md">
+            Want to use this widget in other domain? <br /> Follow these steps.
+          </Text>
+        </HStack>
         <ProWrapper>
           <Stack gap={[5, 6]}>
             <Domains />
