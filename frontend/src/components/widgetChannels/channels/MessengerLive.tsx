16e2ad0c581bbee470391e2df313a4093acfeacd
shuvomohajan <shuvomohajan@gmail.com>
2024-11-23 17:48:53 +0600

    refactor: clean up components by removing unused imports and commented code

--- frontend/src/components/widgetChannels/channels/MessengerLive.tsx
+++ frontend/src/components/widgetChannels/channels/MessengerLive.tsx
@@ -1,29 +1,9 @@
-import { Link, Text } from '@chakra-ui/react'
-import { flowAtom } from '@globalStates/atoms'
-import { useAtom } from 'jotai'
-import { useEffect } from 'react'
+import { Text } from '@chakra-ui/react'
 
 export default function MessengerLive() {
-  // const [, setFlow] = useAtom(flowAtom)
-
-  // useEffect(() => {
-  //   setFlow((prev) => {
-  //     if (typeof prev.config?.card_config === 'undefined') {
-  //       prev.config.card_config = {}
-  //     }
-  //     prev.config.card_config.isChatWidget = true
-  //   })
-  // }, [])
-
   return (
-    // <Text>
-    //   Make Sure to add the Messenger script to your website.{' '}
-    //   <Link href="https://business.facebook.com/latest/inbox/settings" isExternal textDecoration="underline">
-    //     Learn more
-    //   </Link>
-    // </Text>
     <>
-      <Text p={3} mt={-6} color="red.500">
+      <Text p={3} mt={-6} color="red.500" mb={10}>
         On May 9, 2024, you will no longer be able to access any of the functionality of the Chat Plugin. Effective
         immediately, Chat Plugin in guest mode is no longer available. Other features like m.me links will still be
         available for you to use.
