5ecb5d55921145b5ac0b8590551decb4e103cfd8
shuvomohajan <shuvomohajan@gmail.com>
2025-02-19 16:21:29 +0600

    chore: update version to 1.5.5 and enhance changelog for security improvements

--- backend/app/Config.php
+++ backend/app/Config.php
@@ -21,7 +21,7 @@ class Config
 
     public const VAR_PREFIX = 'bit_assist_';
 
-    public const VERSION = '1.5.4';
+    public const VERSION = '1.5.5';
 
     public const DB_VERSION = '1.0.3';
 
--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
+
 /**
  * Plugin Name: Bit Assist
  * Plugin URI:  https://bitapps.pro/bit-assist
  * Description: WhatsApp,Facebook Messenger chat, click to chat Live Chat Widget,Call button & 30+ social chat support button for customer support with sticky chat button
- * Version:     1.5.4
+ * Version:     1.5.5
  * Author:      Bit Assist - Click to Chat Widget Live Chat Support Chat Button
  * Author URI:  https://bitapps.pro
  * Text Domain: bit-assist
